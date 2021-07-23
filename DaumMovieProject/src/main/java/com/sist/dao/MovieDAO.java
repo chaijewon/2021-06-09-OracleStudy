a5881da5754b4c304ca420bd85d2ebf6ff157f92
chaijewon <vcandjava@nate.com>
2021-07-23 13:38:39 +0900

    1111

---
+++ DaumMovieProject/src/main/java/com/sist/dao/MovieDAO.java
@@ -0,0 +1,120 @@
+package com.sist.dao;
+// 오라클 연결 => CURD가 가능하게 만든다 
+import java.sql.*; 
+import java.util.*;
+/*
+ *   재사용 : 상속(is-s) , 포함(has-a) => 기존의 기능을 변경없이 사용할할때
+ *           ========   ===========
+ *           => 기존의 기능을 변경해서 사용할 경우  
+ */
+public class MovieDAO {
+    private Connection conn; //오라클 연결 
+    private PreparedStatement ps; // SQL문장 오라클에 전송 
+    private final String URL="jdbc:oracle:thin:@localhost:1521:XE";
+    // 드라이버를 통해서 연결 => 드라이버 등록한다 = 클래스 Class.forName("드라이버 클래스")
+    public MovieDAO() // 시작과 동시에 드라이버 등록 
+    {
+    	// 초기화 (드라이버 초기화) => 멤버변수 => 파일읽기,웹 읽기 (초기화 담당 메소드)
+    	try
+    	{
+    		Class.forName("oracle.jdbc.driver.OracleDriver"); // 패키지명.클래스명 => XML(패키지명.클래스)
+    		// 클래스가 없는 경우 => ClassNotFoundException (컴파일 예외처리) 
+    		// =반드시 예외처리를 한다 
+    		// 등록 => 클래스 메모리 할당 => 리플렉션 (핵심:스프링) => 스프링(클래스관리자)
+    	}catch(Exception ex)
+    	{
+    		ex.printStackTrace();
+    	}
+    }
+    // 오라클 연결 ==> conn hr/happy => 자바 conn=DriverManager.getConnection(URL,"","")
+    public void getConnection()
+    {
+    	try
+    	{
+    		conn=DriverManager.getConnection(URL,"hr","happy");
+    		// getConnection() ==> conn hr/happy
+    		// TCP (신뢰성) => 연결지향적 프로토콜 , UDP 
+    		// 전화                            우편 
+    	}catch(Exception ex){}
+    }
+    // 오라클을 닫기 => 접속자당 conn을 한개만 사용이 가능 (XE는 연습용 => 12~15)
+    public void disConnection()
+    {
+    	try
+    	{
+    		// exit => ps.close(),conn.close() 
+    		if(ps!=null) ps.close();
+    		if(conn!=null) conn.close(); // 연결되었다면 종료
+    	}catch(Exception ex) {}
+    }
+    // 기능 설정 
+    // 1. 데이터 첨부 
+    /*
+     *  MNO      NOT NULL NUMBER        
+		CNO               NUMBER        
+		TITLE    NOT NULL VARCHAR2(300) 
+		REGDATE           VARCHAR2(100) 
+		GENRE    NOT NULL VARCHAR2(200) 
+		NATION            VARCHAR2(100) 
+		GRADE    NOT NULL VARCHAR2(50)  
+		TIME     NOT NULL VARCHAR2(30)  
+		SCORE             NUMBER(2,1)   
+		SHOWUSER          VARCHAR2(100) 
+		POSTER   NOT NULL VARCHAR2(260) 
+     */
+    // 두가지 => 직접 처리 , 사이트 읽기 
+    public void daumMovieInsert(DaumMovieVO vo)
+    {
+    	try
+    	{
+    		// 1. 연결
+    		getConnection();
+    		// 2. SQL문장 
+    		String sql="INSERT INTO duam_movie VALUES("
+    				  +"daum_mno_seq.nextval,?,?,?,?,?,?,?,?,?,?,?)";
+    		// Sequence의 단점 
+    		// daum_mno_seq.nextval => SELECT MAX(mno)+1 FROM daum_movie
+    		/*
+    		 *   1
+    		 *   2
+    		 *   3
+    		 *   4
+    		 *   
+    		 *   6 
+    		 *   7 
+    		 *   8  ==> 절대 중복된 숫자가 없다 
+    		 */
+    		// 3. 전송
+    		ps=conn.prepareStatement(sql);
+    		// 4. 값을 채운 후 실행 요청
+    		ps.setInt(1, vo.getCno());
+    		ps.setString(2, vo.getTitle());
+    		ps.setString(3, vo.getRegdate());
+    		ps.setString(4, vo.getGenre());
+    		ps.setString(5, vo.getNation());
+    		ps.setString(6, vo.getGrade());
+    		ps.setString(7, vo.getTime());
+    		ps.setDouble(8, vo.getScore());
+    		ps.setString(9, vo.getShowUser());
+    		ps.setString(10, vo.getPoster());
+    		ps.setString(11, vo.getStory());
+    		
+    		ps.executeUpdate(); // commit()이 포함이 되어 있다 
+    	}catch(Exception ex)
+    	{
+    		ex.printStackTrace();// 에러 처리
+    	}
+    	finally
+    	{
+    		disConnection(); // 연결 해제 
+    	}
+    }
+}
+
+
+
+
+
+
+
+
---
+++ DaumMovieProject/src/main/java/com/sist/manager/DaumMovieManager.java
@@ -0,0 +1,157 @@
+package com.sist.manager;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.sist.dao.*;
+/*
+ *  <div class="poster_info">
+      <a href="/moviedb/main?movieId=143722" class="link_story" data-tiara-layer="poster">
+     가족 같은 회사로 모십니다 베이비 주식회사의 레전드 보스 베이비에서 인생 만렙 CEO가 된 `테드`.베이비인 줄 알았던 조카 `티나`가 알고 보니 베이비 주식회사 소속 임원으로 밝혀진다.뉴 보스 베이비 `티나`의 지시로 다시 베이비로 돌아간 `테드`와 형 `팀`에게 주어진 시간은 48시간!세상을 구하기 위한 미션을 무사히 완수할 수 있을 것인가?
+      </a>
+ */
+public class DaumMovieManager {
+   public static void main(String[] args) {
+	   DaumMovieManager d=new DaumMovieManager();
+	   d.movieAllData();
+   }
+   public void movieAllData()
+   {
+	   try
+	   {
+		   Document doc=Jsoup.connect("https://movie.daum.net/ranking/reservation").get(); // HTML소스를 읽어 온다 
+	       Elements link=doc.select("a.link_story");
+	       Elements story=doc.select("a.link_story");
+	       int cno=1;
+	       /*
+	        *  <div class="info_poster">
+                    <a href="#photoId=1423979" class="thumb_img" data-tiara-layer="poster" data-tiara-copy="메인_포스터">
+            <span class="bg_img" style="background-image:url(
+            https://img1.daumcdn.net/thumb/C408x596/?fname=https%3A%2F%2Ft1.
+            daumcdn.net%2Fmovie%2F88c733527983cbd6ab27e4f5f0673358ecfdf9eb)"></span>
+            <span class="ico_movie ico_zoom"></span>
+            </a>
+	        */
+	       for(int i=0;i<link.size();i++)
+	       {
+	    	   System.out.println("링크주소:"+link.get(i).attr("href"));
+	    	   System.out.println("줄거리:"+story.get(i).text());
+	    	   System.out.println("==================================");
+	    	   Document doc2=Jsoup.connect("https://movie.daum.net"+link.get(i).attr("href")).get();
+	    	   Element poster=doc2.selectFirst("div.info_poster span.bg_img");
+	    	   String img=poster.attr("style");
+	    	   img=img.substring(img.indexOf("(")+1,img.lastIndexOf(")"));
+	    	   System.out.println("이미지:"+img);
+	    	   /*
+	    	    *  <h3 class="tit_movie">
+                <span class="txt_tit">
+                    보스 베이비 2
+                </span>
+                    <span class="txt_tag">
+                        <span class="txt_state">상영중</span>
+                    </span>
+                </h3>
+	    	    */
+	    	   Element title=doc2.selectFirst("h3.tit_movie span.txt_tit");
+	    	   System.out.println("제목:"+title.text());
+	    	   /*
+	    	    *  <div class="detail_cont">
+                  <div class="inner_cont">
+                        <dl class="list_cont">
+                                <dt>개봉</dt>
+                            <dd>2021.07.21</dd>
+                        </dl>
+                    <dl class="list_cont">
+                        <dt>장르</dt>
+                        <dd>애니메이션/가족/어드벤처</dd>
+                    </dl>
+                    <dl class="list_cont">
+                        <dt>국가</dt>
+                        <dd>미국</dd>
+                    </dl>
+                        <dl class="list_cont">
+                        <dt>등급</dt>
+                        <dd>
+                            전체관람가
+                        </dd>
+                        </dl>
+                    <dl class="list_cont">
+                    <dt>러닝타임</dt>
+                    <dd>
+                        107분
+                    </dd>
+                    </dl>
+		            </div>
+		            <div class="inner_cont">
+		                    <dl class="list_cont">
+		                    <dt>평점</dt>
+		                    <dd><span class="ico_movie ico_star"></span>9.6</dd>
+		                    </dl>
+		                    <dl class="list_cont">
+		                        <dt>누적관객</dt>
+		                        <dd>99,422명</dd>
+		                    </dl>
+	    	    */
+	    	   /*
+	    	    *   개봉
+					장르
+					국가
+					등급
+					러닝타임
+					평점
+					누적관객
+	    	    */
+	    	   Elements etc=doc2.select("dl.list_cont dt");
+	    	   for(int j=0;j<etc.size();j++)
+	    	   {
+	    		   //System.out.println(etc.get(j).text());
+	    		   String dt=etc.get(j).text();
+	    		   if(dt.equals("개봉"))
+	    		   {
+	    			   Element regdate=doc2.select("dl.list_cont dd").get(j);
+	    			   System.out.println("개봉일:"+regdate.text());
+	    		   }
+	    		   else if(dt.equals("장르"))
+	    		   {
+	    			   Element genre=doc2.select("dl.list_cont dd").get(j);
+	    			   System.out.println("장르:"+genre.text());
+	    		   }
+	    		   else if(dt.equals("국가"))
+	    		   {
+	    			   Element nation=doc2.select("dl.list_cont dd").get(j);
+	    			   System.out.println("국가:"+nation.text());
+	    		   }
+	    		   else if(dt.equals("등급"))
+	    		   {
+	    			   Element grade=doc2.select("dl.list_cont dd").get(j);
+    			       System.out.println("등급:"+grade.text());
+	    			   
+	    		   }
+	    		   else if(dt.equals("러닝타임"))
+	    		   {
+	    			   Element time=doc2.select("dl.list_cont dd").get(j);
+	    			   System.out.println("시간:"+time.text());
+	    		   }
+	    		   else if(dt.equals("평점"))
+	    		   {
+	    			   Element score=doc2.select("dl.list_cont dd").get(j);
+	    			   System.out.println("평점:"+score.text());
+	    		   }
+	    		   else if(dt.equals("누적관객"))
+	    		   {
+	    			   Element user=doc2.select("dl.list_cont dd").get(j);
+	    			   System.out.println("누적관객:"+user.text());
+	    		   }
+	    	   }
+	       }
+	       
+	   }catch(Exception ex){}
+   }
+}
+
+
+
+
+
+
